a7957f03d3c100a8855700958f8621837c9b7150
Jacob Panyathong <jacobpanyathong1@gmail.com>
2026-06-20 23:26:53 -0500

    used if/raise instead of try/raise statements

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,5 +1,7 @@
 def distance(strand_a, strand_b):
-    try:
-        len(strand_a) == len(strand_b)
-    except:
-        ValueError("Strands must be of equal length.")
+    
+    if len(strand_a) != len(strand_b):
+        
+        raise ValueError("Strands must be of equal length.")
+    
+    return sum(a != b for a, b in zip(strand_a, strand_b))
